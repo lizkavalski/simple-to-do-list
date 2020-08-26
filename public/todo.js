a4444828dcc9ca82d04c073a5281e09537cc6605
lizkavalski <lizkavalski@gmail.com>
2020-08-26 13:59:33 -0700

    trying to get enter to work

--- public/todo.js
+++ public/todo.js
@@ -1,18 +1,23 @@
 'use strict';
 $(document).ready(
   function(){
-    $('#toDoList, #button').click(
-      function(){
+    $('#button').on('keypress || click',
+      function(e){
         let toAdd = $('#ListItemInput').val();
         if(toAdd !== ''){
-          $('#listGroup').append('<li>' + toAdd + ' <button id="deletetask"> X </button></li>');
-          $('#ListItemInput').val('');
+          if(e.which === 13 || e.type === 'click'){
+            $('#listGroup').append('<li>' + toAdd + ' <button id="deletetask"> X </button></li>');
+            $('#ListItemInput').val('');
+          }
         } else{
           alert('Need to put in a task please.');
         }
       });
   }
 );
+
+
+
 $(document).on('click','li',function(){
   $(this).fadeOut('slow');
 });

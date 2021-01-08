b48d8180943e7cfe64539a1883bed8a7fb26cd1a
lizkavalski <lizkavalski@gmail.com>
2021-01-08 13:06:53 -0800

    still working on it

--- public/todo.js
+++ public/todo.js
@@ -1,27 +1,20 @@
 'use strict';
-// window.onload=function(){
-//   let list = localStorage.getItem()
-//   if(list !== null){
-//     $('');
-//   }
-// }
-
-let allTask= localStorage.getItem('list')||[]
 $(document).ready(
   function(){
+    let allTask = [];
     $('#toDoList').on('submit',
       function(e){
         let toAdd = $('#ListItemInput').val();
         e.preventDefault();
         if(toAdd !== ''){
           $('#listGroup').append('<li> <button id="deletetask"> X </button>' + toAdd + ' </li>');
-          let newTask = localStorage.setItem('list', JSON.stringify(toAdd));
-          allTask.push(newTask);
+          allTask.push(toAdd);
           $('#ListItemInput').val('');
           console.log('line 10', toAdd);
         } else{
           alert('Need to put in a task please.');
         }
+        localStorage.setItem('list', JSON.stringify(allTask));
       });
   }
 );

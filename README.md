1d3533e5ab744ca22f88b04a8c6052c11c6b0726
lizkavalski <lizkavalski@gmail.com>
2021-01-09 07:58:56 -0800

    fixed list

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 Creating a simple little to-do list with jquery
 
-<!-- this is just a test add more to see differet-->
--- public/todo.js
+++ public/todo.js
@@ -1,27 +1,30 @@
 'use strict';
+
+let allTask = [];
 $(document).ready(
   function(){
-    let allTask = [];
     $('#toDoList').on('submit',
       function(e){
         let toAdd = $('#ListItemInput').val();
         e.preventDefault();
         if(toAdd !== ''){
-          $('#listGroup').append('<li> <button id="deletetask"> X </button>' + toAdd + ' </li>');
+          $('#listGroup').append('<li> <button class="deletetask"> X </button>' + toAdd + ' </li>');
           allTask.push(toAdd);
           $('#ListItemInput').val('');
-          console.log('line 10', toAdd);
         } else{
           alert('Need to put in a task please.');
         }
         localStorage.setItem('list', JSON.stringify(allTask));
-        // $('#listGroup').html(localStorage.getItem('list'))
       });
   }
 );
 
-$(document).on('click','#deletetask',function(){
+$(document).on('click','.deletetask',function(){
   $(this).parent().fadeOut('slow');
 });
 
+$(document).on('click','.deleteEverything',function(){
+  $('#listGroup').fadeOut('slow');
+  localStorage.clear('list')
+})
 
